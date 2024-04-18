63b305928214cb548b3a8ecb3d80dfa9dea20dad
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-04-18 17:12:31 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2429,14 +2429,14 @@ async def advantage_spell_chok(client, msg):
         )
         try:
             if settings['auto_delete']:
-                await asyncio.sleep()
+                await asyncio.sleep(600)
                 await spell_check_del.delete()
         except KeyError:
                 grpid = await active_connection(str(msg.from_user.id))
                 await save_group_settings(grpid, 'auto_delete', True)
                 settings = await get_settings(msg.chat.id)
                 if settings['auto_delete']:
-                    await asyncio.sleep()
+                    await asyncio.sleep(600)
                     await spell_check_del.delete()
 
                     
